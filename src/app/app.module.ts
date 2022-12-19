6bef3e15311f2ed346596d2e57942148604809c4
hsien <hoanplpyy@hotmail.com>
2022-12-19 22:34:13 +0800

    328: 建立core module來存放注入在app module的service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -10,9 +10,8 @@ import { AuthComponent } from './auth/auth.component';
 import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
-import { RecipeService } from './recipes/recipe.service';
-import { AuthInterceptorService } from './auth/auth-interceptor.service';
+import { CoreModule } from './core.module';
+
 
 
 @NgModule({
@@ -26,16 +25,9 @@ import { AuthInterceptorService } from './auth/auth-interceptor.service';
     RecipesModule,
     ShoppingListModule,
     SharedModule,
+    CoreModule
   ],
-  providers:[
-    ShoppingListService,
-    RecipeService,
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptorService,
-      multi: true
-    }
-  ],
+
   bootstrap: [AppComponent]
 })
 export class AppModule {}
